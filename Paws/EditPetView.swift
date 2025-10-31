14512df0a30e863ab4a2cb544b9c9e154046d2aa
Armanul Momin <armanul.momin@gmail.com>
2025-10-31 15:20:27 +1100

    EditPetView added

---
+++ Paws/EditPetView.swift
@@ -0,0 +1,32 @@
+//
+//  EditPetView.swift
+//  Paws
+//
+//  Created by Arman on 31/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct EditPetView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        do{
+            let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(for: Pet.self, configurations: configuration)
+            let sampleData = Pet(name: "Daisy")
+            
+            return EditPetView()
+                .modelContainer(container)
+        } catch {
+            fatalError("Could not load preview data. \(error.localizedDescription)")
+        }
+    }
+}
